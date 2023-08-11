104cf6c1f4e3a0629ad33114ef2a76bbd22bd381
kshoon99 <kshun08@dongguk.edu>
2023-08-11 22:40:53 +0900

    feat: add the contents practiced in lectures 10(about IOC-DI-01)

---
+++ spring-ioc-and-aop/ioc/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String url = "www.naver.com";
+
+        // 단지 new Base64Encoder()/new UrlEncoder() 만을 변경하여 원하는 기능을 사용할 수 있게 되었다.
+        // Encoder encoder = new Encoder(new Base64Encoder());
+        Encoder encoder = new Encoder(new UrlEncoder());
+        String result = encoder.encode(url);
+        System.out.println(result);
+    }
+}

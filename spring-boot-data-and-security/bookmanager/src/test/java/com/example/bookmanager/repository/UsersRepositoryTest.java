fc272a331839b9afbd72830eb824292e49ad900e
kshoon99 <kshun08@dongguk.edu>
2023-09-03 19:19:15 +0900

    feat: add the contents practiced in lectures 40(about jpa repository method 02)

--- spring-boot-data-and-security/bookmanager/src/test/java/com/example/bookmanager/repository/UsersRepositoryTest.java
+++ spring-boot-data-and-security/bookmanager/src/test/java/com/example/bookmanager/repository/UsersRepositoryTest.java
@@ -6,11 +6,15 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
+
 
 @SpringBootTest
 class UsersRepositoryTest {
@@ -24,23 +28,76 @@ class UsersRepositoryTest {
         usersRepository.save(new Users(null, "dennis", "example02@naver.com", LocalDateTime.now(), LocalDateTime.now()));
         usersRepository.save(new Users(null, "sophia", "example03@naver.com", LocalDateTime.now(), LocalDateTime.now()));
         usersRepository.save(new Users(null, "james", "example04@naver.com", LocalDateTime.now(), LocalDateTime.now()));
-        usersRepository.save(new Users(null, "dennis", "example05@naver.com", LocalDateTime.now(), LocalDateTime.now()));
-
-        Optional<Users> users = usersRepository.findById(1L);
-//        Users users = usersRepository.findById(1L).orElse(null);
-
-//        Users user1 = new Users(null, "asd", "asd@naver.com", LocalDateTime.now(), LocalDateTime.now());
-//        Users user2 = new Users(null, "zxc", "zxc@naver.com", LocalDateTime.now(), LocalDateTime.now());
-//        usersRepository.saveAll(Lists.newArrayList(user1, user2));
-//        List<Users> users = usersRepository.findAll();
+        usersRepository.save(new Users(null, "dennis", "example01@naver.com", LocalDateTime.now(), LocalDateTime.now()));
 
 //        Users users = usersRepository.getOne(3L);
 
 //        List<Users> users = usersRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
 //        List<Users> users = usersRepository.findAllById(Lists.newArrayList(1L,3L,5L));
 
-        // users.forEach(System.out::println);
-        System.out.println(users);
+//        Users user1 = new Users(null, "asd", "asd@naver.com", LocalDateTime.now(), LocalDateTime.now());
+//        Users user2 = new Users(null, "zxc", "zxc@naver.com", LocalDateTime.now(), LocalDateTime.now());
+//        usersRepository.saveAll(Lists.newArrayList(user1, user2));
+//        List<Users> users = usersRepository.findAll();
+
+//        Optional<Users> users = usersRepository.findById(1L);
+//        Users users = usersRepository.findById(1L).orElse(null);
+
+//        usersRepository.flush();
+
+//        long count = usersRepository.count();
+
+//        boolean exists = usersRepository.existsById(1L);
+
+
+//        usersRepository.delete(usersRepository.findById(1L).orElseThrow(RuntimeException::new));
+
+//        usersRepository.deleteById(1L);
+
+//        usersRepository.deleteAll();
+//        usersRepository.deleteAll(usersRepository.findAllById(Lists.newArrayList(1L, 3L)));
+//        usersRepository.deleteInBatch(usersRepository.findAllById(Lists.newArrayList(1L, 3L)));
+//        usersRepository.deleteAllInBatch();
+
+//        // 가져오는 페이지의 번호는 1, 페이지 사이즈는 3 이라고 본다. 또한 페이지는 zero-base 이므로 0 부터 시작한다.
+//        Page<Users> users = usersRepository.findAll(PageRequest.of(1,3));
+//
+//        System.out.println("page : "+users);
+//        // 전체 객체 수
+//        System.out.println("totalElements : " + users.getTotalElements());
+//        // 전체 객체 수를 페이지 사이즈로 나누어 나온 값. 즉 전체 페이지 수를 의미한다.
+//        System.out.println("totalPages : " + users.getTotalPages());
+//        // 현재 가져온 레코드의 번호를 의미함. 즉, 가져온 페이지 수의 번호를 의미한다. 갯수 x 번호 o
+//        // zero-base index 에서 1을 더하고 반환.
+//        System.out.println("numberOfElements : " + users.getNumberOfElements());
+//        // sort 을 반환. sort 상태가 아닌 경우에는 UNSORTED 을 반환한다.
+//        System.out.println("sort : " + users.getSort());
+//        // 페이징할 때 기준이 되는 사이즈를 의미한다.
+//        System.out.println("size : " + users.getSize());
+//
+//        // 내부에 들어있는 유저에 대한 정보를 보기 위해서 사용한다.
+//        users.getContent().forEach(System.out::println);
+
+
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("name")
+                .withMatcher("email", contains());
+
+        Example<Users> example = Example.of(new Users(null, "se", "01", null, null), matcher);
+//        Example<Users> example = Example.of(new Users(null, "se", "01@naver.com", null, null));
+
+        usersRepository.findAll(example).forEach(System.out::println);
+
+//        usersRepository.findAll().forEach(System.out::println);
+
+//        System.out.println(exists);
+
+//        System.out.println(count);
+
+//        users.forEach(System.out::println);
+//        System.out.println(users);
+
+//        usersRepository.findAll().forEach(System.out::println);
     }
 
 }
